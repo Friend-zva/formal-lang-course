5bfde4cafb7b00528253ea649c1297f050eaaeb4
Vladimir Zaikin <friend.zva@yandex.ru>
2025-09-20 23:01:53 +0300

    test: AdjacencyMatrixFA from args and 'intersect_automata()'
    Signed-off-by: Vladimir Zaikin <friend.zva@yandex.ru>

--- tests/test_adjacenct_matrix_fa.py
+++ tests/test_adjacenct_matrix_fa.py
@@ -1,6 +1,11 @@
-from pyformlang.finite_automaton import NondeterministicFiniteAutomaton, DeterministicFiniteAutomaton
+from pyformlang.finite_automaton import (
+    NondeterministicFiniteAutomaton,
+    DeterministicFiniteAutomaton,
+)
+from scipy.sparse import lil_array
 
-from project.adjacency_matrix_fa import AdjacencyMatrixFA
+from project.adjacency_matrix_fa import AdjacencyMatrixFA, intersect_automata
+from project.utils import regex_to_dfa
 
 
 def test_adjacency_matrix_fa_empty():
@@ -8,6 +13,44 @@ def test_adjacency_matrix_fa_empty():
     assert amfa
     assert not amfa.accepts("a")
     assert amfa.is_empty()
+
+
+def test_adjacency_matrix_fa_from_arg():
+    amfa = AdjacencyMatrixFA(
+        states_ids={0: 0, 1: 1, 2: 2, 3: 3},
+        symbols={"a", "b", "c"},
+        start_states={0},
+        final_states={2, 3},
+        adjacency_matrices=dict(
+            [
+                (
+                    "a",
+                    lil_array(
+                        [[1, 1, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]],
+                        dtype=bool,
+                    ),
+                ),
+                (
+                    "b",
+                    lil_array(
+                        [[0, 1, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0]],
+                        dtype=bool,
+                    ),
+                ),
+                (
+                    "c",
+                    lil_array(
+                        [[0, 0, 1, 0], [0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0]],
+                        dtype=bool,
+                    ),
+                ),
+            ]
+        ),
+    )
+
+    assert not amfa.accepts("a")
+    assert not amfa.is_empty()
+    assert amfa.accepts("aaacbcaac")
 
 
 def test_adjacency_matrix_fa_from_dfa():
@@ -51,3 +94,12 @@ def test_adjacency_matrix_fa_from_nfa():
     amfa = AdjacencyMatrixFA(nfa)
     assert not amfa.is_empty()
     assert amfa.accepts("aabcb")
+
+
+def test_intersect_automata():
+    dfa1 = AdjacencyMatrixFA(regex_to_dfa("(a a)*"))
+    dfa2 = AdjacencyMatrixFA(regex_to_dfa("a*b*"))
+    dfa = intersect_automata(dfa1, dfa2)
+
+    assert dfa
+    assert dfa.accepts("aa")
