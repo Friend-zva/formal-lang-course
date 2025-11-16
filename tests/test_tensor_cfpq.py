9e27aafb7e2d04f10b3cf033d779ba1b2aa1780f
Vladimir Zaikin <friend.zva@yandex.ru>
2025-11-16 21:18:13 +0300

    test: CFPQ functions
    Signed-off-by: Vladimir Zaikin <friend.zva@yandex.ru>

---
+++ tests/test_tensor_cfpq.py
@@ -0,0 +1,55 @@
+from pyformlang.cfg import CFG, Variable, Terminal, Production, Epsilon
+from networkx import DiGraph
+
+
+from project.tensor_cfpq import cfg_to_rsm, tensor_based_cfpq
+from project.matrix_cfpq import matrix_based_cfpq
+
+
+def test_tensor_based_cfpq():
+    graph = DiGraph()
+    graph.add_node(0, start_node=True, final_node=True)
+    graph.add_edges_from(
+        [
+            (0, 0, {"label": "a"}),
+            (0, 1, {"label": "b"}),
+            (1, 0, {"label": "b"}),
+            (1, 1, {"label": "a"}),
+        ]
+    )
+
+    var_s = Variable("S")
+    t_a = Terminal("a")
+    t_b = Terminal("b")
+    cfg = CFG(
+        variables={var_s},
+        terminals={t_a, t_b},
+        productions={
+            Production(var_s, [t_a, var_s, t_b]),
+            Production(var_s, [Epsilon()]),
+            Production(var_s, [t_a, t_b]),
+        },
+        start_symbol=var_s,
+    )
+
+    rsm = cfg_to_rsm(cfg)
+    assert tensor_based_cfpq(rsm, graph, {0}, {0}) == {(0, 0)}
+
+
+def test_cfpqs():
+    graph = DiGraph()
+    graph.add_node(0, start_node=True, final_node=True)
+    graph.add_edges_from(
+        [
+            (0, 0, {"label": "a"}),
+            (0, 0, {"label": "b"}),
+        ]
+    )
+
+    cfg = CFG.from_text("S -> ε | a S | b S")
+    rsm = cfg_to_rsm(cfg)
+
+    assert tensor_based_cfpq(rsm, graph, {0}, {0}) == matrix_based_cfpq(
+        cfg, graph, {0}, {0}
+    )
+    assert tensor_based_cfpq(rsm, graph, {0}, {0}) == {(0, 0)}
